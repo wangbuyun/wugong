2587c1fed5d801025ce8829d8bf891897e3b3ec7
wangbuyun <286330134@qq.com>
2018-06-14 20:25:17 +0800

    更新1

--- src/com/atguigu/Dugujiujian.java
+++ src/com/atguigu/Dugujiujian.java
@@ -3,8 +3,9 @@ package com.atguigu;
 public class Dugujiujian {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("123123123123");
+		System.out.println("426543645756");
+		System.out.println("4253464575665");
 	}
 
 }
